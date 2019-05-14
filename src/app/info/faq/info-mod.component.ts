8aa3252fbc041261df0e6f5a342d58c3687aa08e
Ahmun <50595344+Ahmun@users.noreply.github.com>
2019-05-14 22:17:23 +0100

    Create info-mod.component.ts

---
+++ src/app/info/faq/info-mod.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-info-faq',
+  templateUrl: './info-faq.component.html',
+  styleUrls: ['./info-faq.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class InfoFaqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
